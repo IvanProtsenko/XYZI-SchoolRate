ce1f2a4bea2ad5f99bb8e014c2f8d178d0f80811
IvanProtsenko <ivnprotsenko@gmail.com>
2018-11-11 15:55:27 +0300

    lol

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'insider/courses';
+    protected $redirectTo = 'main';
 
     /**
      * Create a new controller instance.
