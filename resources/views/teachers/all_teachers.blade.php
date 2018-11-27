678bc31902683d48809776882e598cb8db5c84d3
IvanProtsenko <ivnprotsenko@gmail.com>
2018-11-28 01:59:16 +0300

    a bit more design in teacher_view

--- resources/views/teachers/all_teachers.blade.php
+++ resources/views/teachers/all_teachers.blade.php
@@ -5,36 +5,34 @@
     <div class="row" style = "margin-top: 10px">
         <div class="col-md-8">
             @foreach($teachers as $teacher)
-                            <div class="card-group" style="margin-left: 30px">
-                                <div class="card" style="border: 1px solid grey">
-                                    <div class="card-footer">
-                                        <div class="float-left" style="margin-bottom: -33px">
-                                            <a href="{{url('/profile'.$teacher->id)}}">
-                                                <h4><b>{{$teacher->name}}</b></h4></a>
-                                        </div>
-                                        <div class="float-right" style="margin-right:15px">
-                                            <b>{{$teacher->subject}}</b>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="card-body">
-                                                <div class="float-left">
-                                                    Дата рождения: {{$teacher->age==null?'':$teacher->age->format('Y-m-d')}}
-                                                </div>
-                                                <div class="text-center" style="margin-bottom: -33px">
-                                                    Стаж работы: {{$teacher->stage}} года
-                                                </div>
-                                                <div class="float-right">
-                                                    {{"??"}}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                <div class="card" style="margin-left: 30px;">
+                    <div class="card-footer">
+                        <div class="float-left" style="margin-bottom: -33px">
+                            <a href="{{url('/profile'.$teacher->id)}}">
+                                <h4><b>{{$teacher->name}}</b></h4></a>
+                        </div>
+                        <div class="float-right" style="margin-right:15px">
+                            <b>{{$teacher->subject}}</b>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="card-body">
+                                <div class="float-left">
+                                    Дата рождения: {{$teacher->age==null?'':$teacher->age->format('Y-m-d')}}
+                                </div>
+                                <div class="text-center" style="margin-bottom: -33px">
+                                    Стаж работы: {{$teacher->stage}} года
+                                </div>
+                                <div class="float-right">
+                                    {{"??"}}
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
                 <br>
-                @endforeach
+            @endforeach
         </div>
         <div class="col-md-4">
             <div class="card" style="margin-right: 30px">
--- resources/views/teachers/teacher_view.blade.php
+++ resources/views/teachers/teacher_view.blade.php
@@ -1,29 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card" style="border: 1px solid grey">
-        <div class="card-footer">
+    <div class="card-group">
+        <div class="col-md-4">
+            <div class="card" style="margin-left: 30px; margin-right: 50px; width: 400px;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and ma
+                ke up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    <div class="card" style="margin-right: 30px">
+        <div class="card-footer text-white bg-dark">
             <div class="float-left" style="margin-bottom: -33px">
                 <h4><b>{{$teachers->name}}</b></h4>
             </div>
             <div class="float-right" style="margin-right:15px">
-                <b>{{$teachers->email}}</b>
+                <h5>Предмет: <b>{{$teachers->subject}}</b></h5>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="card-body">
-                    <div class="float-left">
-                        {{$teachers->status}}
-                    </div>
-                    <div class="text-center" style="margin-bottom: -33px">
-                        {{$teachers->stage}}, {{$teachers->age}}
-                    </div>
-                    <div class="float-right">
-                        {{$teachers->subject}}
-                    </div>
-                </div>
+        <div class="card-body">
+            <div class="float-left">
+                Email: <b>{{$teachers->email}}</b>
+            </div>
+            <div class="text-center" style="margin-bottom: -33px">
+                Стаж работы: {{$teachers->stage}}, Дата рождения: {{$teachers->age->format('Y-m-d')}}
+            </div>
+            <div class="float-right">
+                {{$teachers->status}}
             </div>
         </div>
+    </div>
     </div>
 @endsection
