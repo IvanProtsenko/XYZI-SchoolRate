3e3f064e41b77d61d53f2e7ee1718a4f4e408b41
IvanProtsenko <ivnprotsenko@gmail.com>
2018-12-07 15:35:24 +0300

    добавлены и исправлены: переход на главную страницу по щелчку на SchoolRate форма оставления комментария выше предыдущих отзывов только модератор видит id написавших отзывы

--- resources/views/teachers/teacher_view.blade.php
+++ resources/views/teachers/teacher_view.blade.php
@@ -11,50 +11,29 @@
                 </div>
             </div>
         </div>
-    <div class="card" style="margin-right: 30px">
-        <div class="card-footer text-white bg-dark">
-            <div class="float-left" style="margin-bottom: -33px">
-                <h4><b>{{$teacher->name}}</b></h4>
+        <div class="card" style="margin-right: 30px">
+            <div class="card-footer text-white bg-dark">
+                <div class="float-left" style="margin-bottom: -33px">
+                    <h4><b>{{$teacher->name}}</b></h4>
+                </div>
+                <div class="float-right" style="margin-right:15px">
+                    <h5>Предмет: <b>{{$teacher->subject}}</b></h5>
+                </div>
             </div>
-            <div class="float-right" style="margin-right:15px">
-                <h5>Предмет: <b>{{$teacher->subject}}</b></h5>
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="float-left">
-                Email: <b>{{$teacher->email}}</b>
-            </div>
-            <div class="text-center" style="margin-bottom: -33px">
-                Стаж работы: {{$teacher->stage}}, Дата рождения: {{$teacher->age->format('Y-m-d')}}
-            </div>
-            <div class="float-right">
-                {{$teacher->status}}
+            <div class="card-body">
+                <div class="float-left">
+                    Email: <b>{{$teacher->email}}</b>
+                </div>
+                <div class="text-center" style="margin-bottom: -33px">
+                    Стаж работы: {{$teacher->stage}}, Дата рождения: {{$teacher->age->format('Y-m-d')}}
+                </div>
+                <div class="float-right">
+                    {{$teacher->status}}
+                </div>
             </div>
         </div>
     </div>
 
-    </div>
-    @foreach($reviews as $review)
-        @if($review->id_send == \Auth::User()->id || \Auth::User()->status == "moderator")
-        <div class="card" style="margin-right: 30px; margin-left: 510px; margin-top: 20px;">
-            <div class="card-footer text-white bg-dark">
-                <div class="float-left">
-                    <h6>Дата создания комментария: <b>{{$review->created_at}}</b></h6>
-                </div>
-                <div class="float-right">
-                    <h6 style="margin-left: -20px">Id отправящего: <b>{{$review->id_send}}</b></h6>
-                    @if($review->id_send == \Auth::User()->id || \Auth::User()->status == "moderator")
-                        <a href={{"/profile$teacher->id/delete_rev/$review->id"}}>
-                            <img src="https://png.icons8.com/windows/50/000000/cancel.png" width="25px"></a>
-                    @endif
-                </div>
-            </div>
-            <div class="card-body">
-                {{$review->text}}
-            </div>
-        </div>
-        @endif
-    @endforeach
     @if(\Auth::User()->status != "moderator")
     <div style="margin-right: 30px; margin-left: 510px; margin-top: 20px; border: 2px solid blue; padding: 10px">
         <form action="{{url('/profile'.$teacher->id.'/review/')}}" method="post">
@@ -67,4 +46,28 @@
         </form>
     </div>
     @endif
+
+    @foreach($reviews as $review)
+        @if($review->id_send == \Auth::User()->id || \Auth::User()->status == "moderator")
+        <div class="card" style="margin-right: 30px; margin-left: 510px; margin-top: 20px;">
+            <div class="card-footer text-white bg-dark">
+                <div class="float-left">
+                    <h6>Дата создания комментария: <b>{{$review->created_at}}</b></h6>
+                </div>
+                <div class="float-right">
+                    @if(\Auth::User()->status == "moderator")
+                    <h6 style="margin-left: -20px">Id отправящего: <b>{{$review->id_send}}</b></h6>
+                    @endif
+                    @if($review->id_send == \Auth::User()->id || \Auth::User()->status == "moderator")
+                        <a href={{"/profile$teacher->id/delete_rev/$review->id"}}>
+                            <img src="https://png.icons8.com/windows/50/000000/cancel.png" width="25px"></a>
+                    @endif
+                </div>
+            </div>
+            <div class="card-body">
+                {{$review->text}}
+            </div>
+        </div>
+        @endif
+    @endforeach
 @endsection
